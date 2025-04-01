6da7c7ac34ac9d33586a60661f52714891df3c0d
Jangwoo Lee <jangwoo.c.24@gmail.com>
2025-04-01 02:28:40 -0400

    Parse shampoo URLs
    Instead of parsing product info from the shampoo list page, parse URLs only since all other info can be found on individual product pages
    Rename ultascraping_products.py to ultascraping_URLs.py
    TODO: Add other product categories (Conditioner, etc), and save results to JSON (separate by type)
    TODO: Write separate program to parse info per-product based on saved JSON data

---
+++ ultascraping_URLs.py
@@ -0,0 +1,53 @@
+from bs4 import BeautifulSoup
+import requests
+from selenium import webdriver
+
+baseShampooURL = "https://www.ulta.com/shop/hair/shampoo-conditioner/shampoo"
+# baseConditionerURL = "https://www.ulta.com/shop/hair/shampoo-conditioner/conditioner"
+pageURL = "?page="
+
+def scrapeProductPage():
+	return
+
+def scrapeProductsList(baseURL, pageN = 1):
+	r = requests.get(baseURL + pageURL + str(pageN))
+	print(r.status_code)
+
+	productURLs = []
+
+	# file = open('ulta_shampoos.csv', 'w', newline='')
+	# writer = csv.writer(file)
+	# csvheaders = ['Brand', 'Name', 'Review Content', 'Price', 'URL', 'ImgURL']
+	# writer.writerow(csvheaders)
+
+
+	# chrome_options = webdriver.ChromeOptions()
+	# chrome_options.add_argument("--headless=new") #can remove =new for old headless (pre-109 chrome) mode
+	# driver = webdriver.Chrome(options=chrome_options)
+	# driver.get(URLname)
+
+
+	# time.sleep(0.1) #wait for images to lazy load
+
+	# html = driver.page_source
+	html = r.content
+	soupnames = BeautifulSoup(html, 'html5lib')
+
+	namecol = soupnames.find('ul', attrs={'data-test':'products-list'})
+	if namecol:
+		productAs = namecol.find_all('a', attrs={'class':'pal-c-Link pal-c-Link--primary pal-c-Link--default'})
+		for a in productAs:
+			if a.find('div', attrs={'class':'ProductCard__rating'}):
+				productURLs.append(a['href'])
+	return productURLs
+
+def scrapeUltaShampoos():
+	allShampooURLs = []
+	for i in range(1,12):
+		tempList = scrapeProductsList(baseShampooURL, pageN=i)
+		print(tempList)
+		allProductURLs += tempList
+	print(allShampooURLs)
+
+
+scrapeUltaShampoos()
